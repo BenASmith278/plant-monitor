f02d187c8182ffd20f6a18fe197537f48dff714e
Benjamin Smith <benasmith278@gmail.com>
2025-04-28 04:55:59 -0400

    dashboard working

--- smartgarden/plantmonitor/admin.py
+++ smartgarden/plantmonitor/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Measurement, Warning
+
 # Register your models here.
+admin.site.register(Measurement)
+admin.site.register(Warning)
--- smartgarden/plantmonitor/views.py
+++ smartgarden/plantmonitor/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+from .models import Measurement, Warning
+
 
 # Create your views here.
+def index(request):
+    context = {}
+    return render(request, "plantmonitor/dashboard.html", context)
+
+
+# class DetailView(DetailView):
+#     model = Measurement
+#     template_name = 'measurement_detail.html'
+#     context_object_name = 'measurement'
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['warnings'] = Warning.objects.filter(measurement=self.object)
+#         return context
