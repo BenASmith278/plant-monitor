9cd410cdb0160d1929ba2c04f41921e0ac55406f
Jia22k <jxk1403@case.edu>
2025-04-30 21:09:51 -0400

    added data visual for last 2 hours every 3 min will change for demo to something more realistic

--- smartgarden/plantmonitor/urls.py
+++ smartgarden/plantmonitor/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("api/system-config/", views.get_system_config, name="get_system_config"),
     path("api/trigger-pump/", views.trigger_pump, name="trigger_pump"),
     path("api/poll-pump/", views.poll_pump_command, name="poll_pump"),
+    path("api/chart-data/", views.get_chart_data, name="chart_data"),
     path("", views.index, name="index"),
 ]
